05a6a7ece672bf58dd4a9c8c7ab064dfa1b8867f
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-10 22:29:58 +0100

    :tada: feat: Creation of 2 channels 24.05 + unstable for home manager and nix

---
+++ configuration.nix
@@ -0,0 +1,66 @@
+{ config, pkgs, pkgs-unstable, ... }:
+
+{
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./modules/nixos/modules.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "kashyyyk"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    lc_address = "en_US.utf-8"; # Use US locale for address formatting
+    lc_identification =
+      "en_US.utf-8"; # Use US locale for identification formatting
+    lc_measurement = "en_US.utf-8"; # Use US locale for measurement formatting
+    lc_monetary = "en_US.utf-8"; # Use US locale for monetary formatting
+    lc_name = "en_US.utf-8"; # Use US locale for name formatting
+    lc_numeric = "en_US.utf-8"; # Use US locale for numeric formatting
+    lc_paper = "en_US.utf-8"; # Use US locale for paper size (e.g., letter)
+    lc_telephone =
+      "en_US.utf-8"; # Use US locale for telephone number formatting
+    lc_time = "en_US.utf-8"; # Use US locale for time formatting
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "intl";
+  };
+
+  users.users.thomas = {
+    isNormalUser = true;
+    description = "thomas";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = (with pkgs;
+    [
+      git
+    ])
+    ++
+    (with pkgs-unstable;
+    []
+    );
+  dwm.enable=true;
+  WM.enable=true;
+  pipewire.enable=true;
+  
+  system.stateVersion = "24.05"; # Did you read the comment?
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+}
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Thomas Derudder Flakes";
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOs/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      ...
+    }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      pkgs-unstable = import nixpkgs-unstable {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    in
+    {
+      nixosConfigurations = {
+        kashyyyk = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+          specialArgs = {
+            inherit pkgs-unstable;
+          };
+        };
+      };
+      homeConfigurations = {
+        thomas = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./modules/home-manager/home.nix ];
+          extraSpecialArgs = {
+            inherit pkgs-unstable;
+          };
+        };
+      };
+    };
+}
