2018ebdd667553d47b0c33257ce59c2ce401199e
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-12 00:11:19 +0100

    :+1: fix: downgrade to pkgs

--- modules/home-manager/app/3d/orca-slicer.nix
+++ modules/home-manager/app/3d/orca-slicer.nix
@@ -1,5 +1,5 @@
 {
-  pkgs-unstable,
+  pkgs,
   lib,
   config,
   ...
@@ -9,5 +9,5 @@
   options = {
     orca.enable = lib.mkEnableOption "enables orca slicer";
   };
-  config = lib.mkIf config.orca.enable { home.packages = with pkgs-unstable; [ orca-slicer ]; };
+  config = lib.mkIf config.orca.enable { home.packages = with pkgs; [ orca-slicer ]; };
 }
