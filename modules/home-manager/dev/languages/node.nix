db7e15d07bf5d389aa9cb6f7ac1dc1d1a8b5ab86
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-11 15:53:49 +0100

    :sparkles: feat: Add pretierd

--- modules/home-manager/dev/languages/node.nix
+++ modules/home-manager/dev/languages/node.nix
@@ -10,5 +10,10 @@
     node.enable = lib.mkEnableOption "enable node";
   };
 
-  config = lib.mkIf config.node.enable { home.packages = with pkgs; [ nodejs_22 ]; };
+  config = lib.mkIf config.node.enable {
+    home.packages = with pkgs; [
+      nodejs_22
+      prettierd
+    ];
+  };
 }
