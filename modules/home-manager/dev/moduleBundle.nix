b2f4e20e2083207ebf0e238853b2dde491f3de07
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-12 00:42:07 +0100

    :sparkles: feat: Moving to default

--- modules/home-manager/dev/moduleBundle.nix
+++ modules/home-manager/dev/moduleBundle.nix
@@ -4,7 +4,7 @@
     ./terminal/moduleBundle.nix
     ./git/moduleBundle.nix
     ./editor/moduleBundle.nix
-    ./languages/moduleBundle.nix
-    ./utility/moduleBundle.nix
+    ./languages
+    ./utility
   ];
 }
