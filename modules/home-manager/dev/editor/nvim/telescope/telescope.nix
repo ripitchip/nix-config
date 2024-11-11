e85ec3059c6db33f36f58874782632d5a54675f8
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-11 15:30:35 +0100

    :recycle: refactor: Formatting

--- modules/home-manager/dev/editor/nvim/telescope/telescope.nix
+++ modules/home-manager/dev/editor/nvim/telescope/telescope.nix
@@ -1,46 +1,52 @@
-{ pkgs, pkgs-unstable, lib, config, ... }: {
+{
+  pkgs,
+  pkgs-unstable,
+  lib,
+  config,
+  ...
+}:
+{
 
-  options = { nvim.telescope.enable = lib.mkEnableOption "enables telescope in neovim"; };
+  options = {
+    nvim.telescope.enable = lib.mkEnableOption "enables telescope in neovim";
+  };
   config = lib.mkIf (config.nvim.enable && config.nvim.telescope.enable) {
 
     home.packages = with pkgs; [
       ripgrep
+      fd
     ];
 
-    programs.neovim = let
-      toLua = str: ''
-        lua << EOF
-        ${str}
-        EOF
-      '';
-      toLuaFile = file: ''
-        lua << EOF
-        ${builtins.readFile file}
-        EOF
-      '';
-    in {
-      plugins = with pkgs-unstable.vimPlugins; [
-        {
-          plugin = telescope-nvim;
-          config = toLuaFile ./telescope.lua;
-        }
-        telescope-undo-nvim
-        telescope-fzf-native-nvim
-        vim-bookmarks
-        telescope-vim-bookmarks-nvim
-        telescope-coc-nvim
-        telescope-file-browser-nvim
-        telescope-project-nvim
-        telescope-media-files-nvim
-        telescope_hoogle
-        telescope-live-grep-args-nvim
-        harpoon2
-        trouble-nvim
-      ];
-      extraLuaConfig = ''
-        ${builtins.readFile ./keymap.lua}
-      '';
-    };
+    programs.neovim =
+      let
+        toLuaFile = file: ''
+          lua << EOF
+          ${builtins.readFile file}
+          EOF
+        '';
+      in
+      {
+        plugins = with pkgs-unstable.vimPlugins; [
+          {
+            plugin = telescope-nvim;
+            config = toLuaFile ./telescope.lua;
+          }
+          telescope-undo-nvim
+          telescope-fzf-native-nvim
+          vim-bookmarks
+          telescope-vim-bookmarks-nvim
+          telescope-coc-nvim
+          telescope-file-browser-nvim
+          telescope-project-nvim
+          telescope-media-files-nvim
+          telescope_hoogle
+          telescope-live-grep-args-nvim
+          harpoon2
+          trouble-nvim
+        ];
+        extraLuaConfig = ''
+          ${builtins.readFile ./keymap.lua}
+        '';
+      };
   };
 }
-
