14265bf44b09caa216053723826f13cf34859464
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-11 13:06:36 +0100

    :+1: fix: Update to not specify home path

--- modules/home-manager/app/media/music/mpd.nix
+++ modules/home-manager/app/media/music/mpd.nix
@@ -4,24 +4,25 @@
   config,
   ...
 }:
-{
 
+{
   options = {
-    mpd.enable = lib.mkEnableOption "enables mpd";
+    mpd.enable = lib.mkEnableOption "Enable MPD service";
   };
 
   config = lib.mkIf config.mpd.enable {
     services.mpd = {
       enable = true;
-      musicDirectory = "/home/thomas/music/";
+      # Use `config.home.homeDirectory` for the user's home directory
+      musicDirectory = "${config.home.homeDirectory}/music";
       extraConfig = ''
         audio_output {
           type            "pipewire"
           name            "PipeWire Sound Server"
         }
-
       '';
     };
+
     home.packages = with pkgs; [
       mpc-cli
       ncmpcpp
