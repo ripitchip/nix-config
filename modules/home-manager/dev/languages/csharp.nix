4ad806e0af197f612d9342c767af2208754fa4ec
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-11 16:14:53 +0100

    :+1: fix: Fox and add csharpier

--- modules/home-manager/dev/languages/csharp.nix
+++ modules/home-manager/dev/languages/csharp.nix
@@ -11,12 +11,9 @@
   };
 
   config = lib.mkIf config.csharp.enable {
-    home.packages =
-      with pkgs.dotnetCorePackages;
-      combinePackages [
-        sdk_8_0
-        runtime_8_0
-        aspnetcore_8_0
-      ];
+    home.packages = with pkgs.dotnetCorePackages; [
+      sdk_9_0
+      csharpier
+    ];
   };
 }
