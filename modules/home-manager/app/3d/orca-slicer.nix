bfa1d737729bbc7e960c82193af47052afaa41a5
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-11 16:24:42 +0100

    :sparkles: feat: Add orca slicer

---
+++ modules/home-manager/app/3d/orca-slicer.nix
@@ -0,0 +1,13 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+{
+  options = {
+    orca.enable = lib.mkEnableOption "enables orca slicer";
+  };
+  config = lib.mkIf config.orca.enable { home.packages = with pkgs; [ orca-slicer ]; };
+}
