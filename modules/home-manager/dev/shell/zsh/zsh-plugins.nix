0cf1d01dd603f3026c751e41ab4fdf4d02b38480
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-12 15:33:51 +0100

    :sparkles: feat: Update plugins sha and url

--- modules/home-manager/dev/shell/zsh/zsh-plugins.nix
+++ modules/home-manager/dev/shell/zsh/zsh-plugins.nix
@@ -1,108 +1,101 @@
-# zsh-plugins.nix
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
-  # Define each plugin using fetchgit
   fzfZshPlugin = pkgs.fetchgit {
     url = "https://github.com/unixorn/fzf-zsh-plugin.git";
-    rev = "main";
-    ref = "refs/heads/main";
+    sha256 = "Ubxakc8DwJoy49o3B5L+t5vZw3KA2VWvlAPGWER8J2A="; # Set to an invalid value so Nix recalculates it
   };
 
   zshAutopair = pkgs.fetchgit {
     url = "https://github.com/hlissner/zsh-autopair.git";
-    rev = "main";
-    ref = "refs/heads/main";
+    sha256 = "3zvOgIi+q7+sTXrT+r/4v98qjeiEL4Wh64rxBYnwJvQ=";
   };
 
   zshAutosuggestions = pkgs.fetchgit {
-    url = "git@github.com:zsh-users/zsh-autosuggestions.git";
-    rev = "main";
-    ref = "refs/heads/main";
+    url = "https://github.com/zsh-users/zsh-autosuggestions.git";
+    sha256 = "B+Kz3B7d97CM/3ztpQyVkE6EfMipVF8Y4HJNfSRXHtU=";
   };
 
   zshCompletions = pkgs.fetchgit {
-    url = "git@github.com:zsh-users/zsh-completions.git";
-    rev = "main";
-    ref = "refs/heads/main";
+    url = "https://github.com/zsh-users/zsh-completions.git";
+    sha256 = "ibdEDgyUiJ84OUdlwrlvedlySE8uwQfNqQ6HEM+QCLM=";
   };
 
   zshLsd = pkgs.fetchgit {
-    url = "git@github.com:wintermi/zsh-lsd.git";
-    rev = "main";
-    ref = "refs/heads/main";
+    url = "https://github.com/wintermi/zsh-lsd.git";
+    sha256 = "3dQoaDwgrO9kV4g+yRxy6gVug6wPo6/MNTPwvK2wIJ4=";
   };
 
   zshSyntaxHighlighting = pkgs.fetchgit {
-    url = "git@github.com:zsh-users/zsh-syntax-highlighting.git";
-    rev = "main";
-    ref = "refs/heads/main";
+    url = "https://github.com/zsh-users/zsh-syntax-highlighting.git";
+    sha256 = "Vt2yKzMRJ34FBFPKrN+GJBZYmBt5ASArrs1dkZcIQmI=";
   };
 
   youShouldUse = pkgs.fetchgit {
-    url = "git@github.com:MichaelAquilina/zsh-you-should-use.git";
-    rev = "main";
-    ref = "refs/heads/main";
+    url = "https://github.com/MichaelAquilina/zsh-you-should-use.git";
+    sha256 = "+3iAmWXSsc4OhFZqAMTwOL7AAHBp5ZtGGtvqCnEOYc0=";
   };
 
   condaZshCompletion = pkgs.fetchgit {
-    url = "git@github.com:conda-incubator/conda-zsh-completion.git";
-    rev = "main";
-    ref = "refs/heads/main";
+    url = "https://github.com/conda-incubator/conda-zsh-completion.git";
+    sha256 = "lphlOmdy8pHSNzupSnuE9KvLT303UEbVRGcHsxwakIU=";
   };
 
   zshAutocomplete = pkgs.fetchgit {
-    url = "git@github.com:marlonrichert/zsh-autocomplete.git";
-    rev = "main";
-    ref = "refs/heads/main";
+    url = "https://github.com/marlonrichert/zsh-autocomplete.git";
+    sha256 = "o8IQszQ4/PLX1FlUvJpowR2Tev59N8lI20VymZ+Hp4w=";
   };
 
 in
 {
   home.file = {
+    ".config/zsh/custom/themes" = {
+      source = ./themes;
+      recursive = true;
+    };
+    ".config/zsh/custom/plugins" = {
+      source = ./plugins;
+      recursive = true;
+    };
     ".config/zsh/custom/plugins/fzf-zsh-plugin" = {
       source = fzfZshPlugin;
       recursive = true;
-      directory = true;
     };
     ".config/zsh/custom/plugins/zsh-autopair" = {
       source = zshAutopair;
       recursive = true;
-      directory = true;
     };
     ".config/zsh/custom/plugins/zsh-autosuggestions" = {
       source = zshAutosuggestions;
       recursive = true;
-      directory = true;
     };
     ".config/zsh/custom/plugins/zsh-completions" = {
       source = zshCompletions;
       recursive = true;
-      directory = true;
     };
     ".config/zsh/custom/plugins/zsh-lsd" = {
       source = zshLsd;
       recursive = true;
-      directory = true;
     };
     ".config/zsh/custom/plugins/zsh-syntax-highlighting" = {
       source = zshSyntaxHighlighting;
       recursive = true;
-      directory = true;
     };
     ".config/zsh/custom/plugins/you-should-use" = {
       source = youShouldUse;
       recursive = true;
-      directory = true;
     };
     ".config/zsh/custom/plugins/conda-zsh-completion" = {
       source = condaZshCompletion;
       recursive = true;
-      directory = true;
     };
     ".config/zsh/custom/plugins/zsh-autocomplete" = {
       source = zshAutocomplete;
       recursive = true;
-      directory = true;
     };
   };
 }
