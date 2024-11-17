f030bcf184999dd6e46fbeba861601e1068c7a84
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-17 14:16:30 +0100

    :sparkles: feat: Update lsp and treesitter

--- modules/home-manager/dev/editor/nvim/tree-sitter/tree-sitter.nix
+++ modules/home-manager/dev/editor/nvim/tree-sitter/tree-sitter.nix
@@ -29,7 +29,62 @@
       {
         plugins = with pkgs-unstable.vimPlugins; [
           {
-            plugin = nvim-treesitter.withAllGrammars;
+            plugin = (
+              nvim-treesitter.withPlugins (p: [
+                # Programming Languages
+                p.tree-sitter-arduino
+                p.tree-sitter-bash
+                p.tree-sitter-c
+                p.tree-sitter-cpp
+                p.tree-sitter-c_sharp
+                p.tree-sitter-cmake
+                p.tree-sitter-dart
+                p.tree-sitter-go
+                p.tree-sitter-java
+                p.tree-sitter-javascript
+                p.tree-sitter-lua
+                p.tree-sitter-python
+                p.tree-sitter-ruby
+                p.tree-sitter-rust
+                p.tree-sitter-typescript
+
+                # Markup and Styling Languages
+                p.tree-sitter-html
+                p.tree-sitter-css
+                p.tree-sitter-markdown
+                p.tree-sitter-markdown-inline
+                p.tree-sitter-latex
+                p.tree-sitter-scss
+                p.tree-sitter-toml
+                p.tree-sitter-xml
+                p.tree-sitter-yaml
+                p.tree-sitter-vue
+
+                # Version Control and Configurations
+                p.tree-sitter-git_config
+                p.tree-sitter-gitcommit
+                p.tree-sitter-gitignore
+                p.tree-sitter-gitattributes
+                p.tree-sitter-editorconfig
+                p.tree-sitter-dockerfile
+                p.tree-sitter-helm
+
+                # Miscellaneous
+                p.tree-sitter-astro
+                p.tree-sitter-bibtex
+                p.tree-sitter-desktop
+                p.tree-sitter-mermaid
+                p.tree-sitter-nix
+                p.tree-sitter-proto
+                p.tree-sitter-powershell
+                p.tree-sitter-vim
+                p.tree-sitter-vimdoc
+                p.tree-sitter-json
+                p.tree-sitter-jsonc
+                p.tree-sitter-sql
+              ])
+
+            );
             config = toLuaFile ./treesitter.lua;
           }
 
